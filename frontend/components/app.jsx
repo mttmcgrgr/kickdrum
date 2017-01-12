32e6e3170b0838aafb6fb61dc5f112b5f0ca7a2f
Matt McGregor <mttmcgrgr@gmail.com>
2017-01-12 15:09:39 -0800

    login page styling

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,12 +3,14 @@ import GreetingContainer from './greeting/greeting_container';
 
 const App = ({ children }) => (
   <div>
-    <h1>Kickdrum
+    <h1 className="header-name">Kickdrum
       <img className="logo" src="http://res.cloudinary.com/dccshngpp/image/upload/v1484184165/noun_409558_wimkcq.png"/>
     </h1>
 
     <GreetingContainer />
     { children }
+
+
   </div>
 );
 
