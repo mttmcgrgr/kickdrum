44964b3f1ba4782a592d3d7e1974aacd47d30bbf
Matt McGregor <mttmcgrgr@gmail.com>
2017-01-11 18:47:31 -0800

    logo styling

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,7 +3,10 @@ import GreetingContainer from './greeting/greeting_container';
 
 const App = ({ children }) => (
   <div>
-    <h1>Kickdrum</h1>
+    <h1>Kickdrum
+      <img className="logo" src="http://res.cloudinary.com/dccshngpp/image/upload/v1484184165/noun_409558_wimkcq.png"/>
+    </h1>
+
     <GreetingContainer />
     { children }
   </div>
