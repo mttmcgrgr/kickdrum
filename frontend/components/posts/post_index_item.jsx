13f5934da87ddf433157cec92b9f2f90cce2cd1d
Matt McGregor <mttmcgrgr@gmail.com>
2017-01-13 20:41:59 -0800

    add audio playback on image click

--- frontend/components/posts/post_index_item.jsx
+++ frontend/components/posts/post_index_item.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 
 
+const playSong = (post) => {
+  var audio = new Audio(post.song_url);
+  audio.play();
+}
+
+
 const PostIndexItem = ({ post }) => (
   <li className="content_main">
     <section className="post_header">
@@ -11,7 +17,7 @@ const PostIndexItem = ({ post }) => (
       <div className="container">
         <img className="image" src={post.cover_url}/>
           <div className="middle">
-            <img className="play" src="http://res.cloudinary.com/dccshngpp/image/upload/v1484354473/video-play-3-xxl_hzjck2.png"/>
+            <img className="play" onClick={playSong.bind(null,post)} src="http://res.cloudinary.com/dccshngpp/image/upload/v1484354473/video-play-3-xxl_hzjck2.png"/>
          </div>
      </div>
     </section>
