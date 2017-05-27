7e8adab0a22afbda91f445a8fdbf877c7f50bbce
Matt McGregor <mttmcgrgr@gmail.com>
2017-05-26 19:46:17 -0700

    Change index post

--- frontend/components/posts/post_index_item.jsx
+++ frontend/components/posts/post_index_item.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 
 const PostIndexItem = ( {post, receiveTrack} ) => {
   return (
-  <li className="li-post-index">
+  <li className="li-post-inde">
     <section className="post_header">
       <Link to={`/users/${post.user_id}`}>{post.username}</Link>
     </section>
