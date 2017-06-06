8e6336847fdcde0eaaefc56cd2630c43b4759d08
Matt McGregor <mttmcgrgr@gmail.com>
2017-06-05 18:41:23 -0700

    change z-index on homepage description

--- frontend/components/greeting/home.jsx
+++ frontend/components/greeting/home.jsx
@@ -3,14 +3,15 @@ import HomeFeedContainer from './home_feed_container';
 
 const Home = () => (
   <div className='welcome-descriptions'>
-    <div className='welcome-subcontainer'>
-      <h3>Your Next Favorite Song Is Already Here</h3>
-      <p>And The Next. And The Next.</p>
-    </div>
 
     <div className='welcome-subcontainer'>
       <h3>Listen While You Search</h3>
       <p>Browse Freely Without Interrupting Your Listening</p>
+    </div>
+
+    <div className='welcome-subcontainer'>
+      <h3>Your Next Favorite Song Is Already Here</h3>
+      <p>And The Next. And The Next.</p>
     </div>
 
     <div className='welcome-subcontainer'>
