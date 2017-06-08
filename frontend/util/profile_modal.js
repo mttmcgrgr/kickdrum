07242d33dded219ff5a0af58b01919b8ff034617
Matt McGregor <mttmcgrgr@gmail.com>
2017-06-08 02:47:27 -0700

    fix user modal formatting

--- frontend/util/profile_modal.js
+++ frontend/util/profile_modal.js
@@ -5,20 +5,22 @@ module.exports = {
     left              : 0,
     right             : 0,
     bottom            : 0,
-    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+    backgroundColor: 'rgba(0, 0, 0, .7)',
     width             : '100%',
     height            : '100%'
   },
   content : {
     display        : 'flex',
+    padding        : "0px",
     top            : '50%',
-    left           : '50%',
+    left           : '49%',
     right          : 'auto',
     bottom         : 'auto',
     marginRight    : '-50%',
     transform      : 'translate(-50%, -50%)',
-    width          : '995px',
+    width          : '1050px',
     height         : '549px',
-    zIndex         : 1000
+    zIndex         : 1000,
+    overflow       : "hidden"
   }
 };
