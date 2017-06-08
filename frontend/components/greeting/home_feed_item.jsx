a014996c8c94d222fe7741a41d93dba27ba149dd
Matt McGregor <mttmcgrgr@gmail.com>
2017-06-07 22:13:32 -0700

    fix centering on home feed play button

--- frontend/components/greeting/home_feed_item.jsx
+++ frontend/components/greeting/home_feed_item.jsx
@@ -14,7 +14,7 @@ const HomeFeedItem = ( {post, receiveTrack} ) => (
       <div className="container">
         <img className="session_image" src={post.cover_url}/>
           <div className="middle">
-            <img className="play" onClick={()=> (receiveTrack(post))} src="http://res.cloudinary.com/dccshngpp/image/upload/v1484354473/video-play-3-xxl_hzjck2.png"/>
+            <img className="session-play" onClick={()=> (receiveTrack(post))} src="http://res.cloudinary.com/dccshngpp/image/upload/v1484354473/video-play-3-xxl_hzjck2.png"/>
          </div>
      </div>
   </li>
